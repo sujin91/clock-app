f7c74741c8f407459d5cabe23068d783843f4105
sujin.shin <sujin.shin@nts-corp.com>
2021-03-29 12:48:49 +0900

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+## Clock-App
+
+### 실행방법
+1. `npm install lite-server -g`
+2. `lite-server` 실행
+3. `localhost:3000`, terminal에 표시된 `PORT` 번호 확인 후 접속
+
---
+++ js/App.js
@@ -0,0 +1,11 @@
+// import Controller from './controllers/Controller.js'
+// import Model from './models/Model.js'
+// import View from './views/View.js'
+
+function App() {
+    console.log('app create')
+    // const app = new Controller(new Model(), new View())
+}
+
+new App()
+
